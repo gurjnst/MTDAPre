bbc05142c5332b83103e21fc7222da720d282402
gurjnst <931183629@qq.com>
2026-07-22 01:10:56 +0800

    Add files via upload

---
+++ configs/weather/ms_5_s6_5_625/MTDAPre.py
@@ -0,0 +1,37 @@
+# MISO
+batch_size = 16
+val_batch_size = 16
+epoch = 50
+opt = 'adamw'
+weight_decay = 0.01
+sched = 'cosine'  # onecycle cosine
+lr = 1e-3  #5e-4  1e-3
+
+dataname = 'weather_ms5my_12_5_625'
+method = 'MTDAPre'
+model = 'Target'  # Early Middle Target
+model_config = {
+    # image h w c
+    'height': 32,
+    'width': 64,
+    # SISO 1->1  MISO 5->1  MIMO 5->5
+    'num_channels': 5,
+    'out_channels': 1,
+    # video length in and out
+    'pre_seq': 12,
+    'after_seq': 12,
+    # patch size
+    'patch_size': 4,
+    'dim': 128,
+    'heads': 8,
+    'dim_head': 16,
+    # dropout
+    'dropout': 0.1,
+    'attn_dropout': 0.1,
+    'drop_path': 0.2,
+    'scale_dim': 4,
+    # depth
+    'depth': 1,
+    'Ndepth': 6,
+    'auxdepth': 4,
+}
