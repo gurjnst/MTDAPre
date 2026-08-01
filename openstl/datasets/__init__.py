794787cded444f2f23955dd964fe0c5afef6ec2d
gurjnst <931183629@qq.com>
2026-08-02 00:55:08 +0800

    Add files via upload

---
+++ openstl/datasets/__init__.py
@@ -0,0 +1,10 @@
+from .dataloader_weather import WeatherBenchDataset
+from .dataloader import load_data
+from .dataset_constant import dataset_parameters
+from .pipelines import *
+from .utils import create_loader
+
+__all__ = [
+    'WeatherBenchDataset',
+    'load_data', 'dataset_parameters', 'create_loader'
+]
